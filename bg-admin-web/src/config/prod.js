572ad7cdb1954e3a3ce289a071a51bc8f1d69451
林泽锋 <282245889@qq.com>
2019-07-15 17:11:07 +0800

    测试环境的搭建

--- bg-admin-web/src/config/prod.js
+++ bg-admin-web/src/config/prod.js
@@ -1,4 +1,6 @@
 export const runProdConfig = {
-  baseUrl : 'http://127.0.0.1:8088',
-  imgUrl : 'http://127.0.0.1:80/vcm'
+  baseUrl: 'http://10.10.10.113:8288',
+  imgUrl: 'http://127.0.0.1:80/merJoinResource',
+  refreshTokenUrl: 'http://10.10.10.113:8288/user/refreshToken',
+  socketUrl: 'http://10.10.10.113:9099'
 };
